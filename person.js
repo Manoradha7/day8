cf63294707a5e9347b7ffa9b9e3184bb588f447c
Manoradha7 <91074732+Manoradha7@users.noreply.github.com>
2021-10-10 15:43:22 +0530

    Create person.js

---
+++ person.js
@@ -0,0 +1,21 @@
+// Write a “person” class to hold all the details.
+
+class Person {
+    constructor(fname,lname,age,mob,address){
+        this.fname = fname;
+        this.lname = lname;
+        this.age = age;
+        this.mob = mob;
+        this.address = address;
+    }
+     
+}
+
+person1 = new Person("Mano","karan",21,9233244323,{city:"Trichy",state:"tamilnadu"});
+console.log(person1.address);
+console.log(person1.fname);
+console.log(person1.lname);
+console.log(person1.age);
+console.log(person1.mob);
+
+
