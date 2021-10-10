c428adc90b240145df4a7650d56ef4fcf374c016
Manoradha7 <91074732+Manoradha7@users.noreply.github.com>
2021-10-10 16:02:31 +0530

    Update movies.js

--- movies.js
+++ movies.js
@@ -1,15 +1,18 @@
+//  create movie class that contain movie details
 class Movie{
-    constructor(title,studio,rating="PG"){
+    constructor(title,studio,rating="PG"){ //default value of rating is "PG"
         this.title = title;
         this.studio = studio;
         this.rating = rating;
     }
 }
 
+//A method getPG, which takes an array of base type Movie as its argument
 getPG(moviesArray){
   return moviesArray.filter( this.rating );
 }
 
+// piece of code that creates an instance of the class Movie
 movie1 = new Movie("Casino Royale","Eon Productions","PG13");
 console.log(movie1.title);    // Casino Royale
 console.log(movie1.studio);   // Eon Productions
